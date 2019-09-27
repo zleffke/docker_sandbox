a6db6a5da08b3d5e2bd1bca937ec215384245a10
Zach Leffke <zleffke@vt.edu>
2019-09-27 15:56:06 -0400

    added hack to install gnuradio 3.7 as gnuradio for other OOTMs that depend on gnuradio being installed (not gnuradio37).

--- gnuradio/config_gnuradio.sh
+++ gnuradio/config_gnuradio.sh
@@ -47,36 +47,41 @@ pip install pybombs
 pybombs auto-config
 pybombs config makewidth 8 #change this to match available cores
 #configure prefix
-#in /gnuradio, so should hopefull
 pybombs prefix init /gnuradio/target -a target
-pybombs recipes add-defaults
+pybombs -vvv -t target recipes add-defaults
+
+#---HACK---
+#overwrite gnuradio.lwr from gnuradio37.lwr to install 3.7
+#this should register the install as gnuradio
+#This for other OOTMs that depend on gnuradio
+mv /gnuradio/target/.pybombs/recipes/gr-recipes/gnuradio37.lwr /gnuradio/target/.pybombs/recipes/gr-recipes/gnuradio.lwr 
 
 #install common dependencies, may not be comprehensive
 echo "---Installing Common Dependencies for PyBOMBS---"
-pybombs -vvv install boost doxygen
-pybombs -vvv install libtool autoconf automake
-pybombs -vvv install qwt5 sip lxml
-pybombs -vvv install pygtk pycairo pyqwt5 python-requests six mako numpy
-pybombs -vvv install zeromq python-zmq
-pybombs -vvv install gsl fftw
-pybombs -vvv install libusb alsa
-pybombs -vvv install cppunit liblog4cpp
+pybombs -vvv -p target install boost doxygen
+pybombs -vvv -p target install libtool autoconf automake
+pybombs -vvv -p target install qwt5 sip lxml
+pybombs -vvv -p target install pygtk pycairo pyqwt5 python-requests six mako numpy
+pybombs -vvv -p target install zeromq python-zmq
+pybombs -vvv -p target install gsl fftw
+pybombs -vvv -p target install libusb alsa
+pybombs -vvv -p target install cppunit liblog4cpp
 
 #install UHD and GNU Radio
 echo "---Installing GNU Radio---"
-pybombs -vvv install uhd
-pybombs -vvv install gnuradio37 #don't upgrade to 3.8 yet.
+pybombs -vvv -p target install uhd
+pybombs -vvv -p target install gnuradio #don't upgrade to 3.8 yet.
 
 #Install VCC specific Out of Tree Modules (OOTMs)
 echo "---Installing VCC OOTMs---"
-pybombs recipes add gr-vtgs-recipes git+https://github.com/vt-gs/recipes.git
-pybombs -vvv install gr-kiss    #kiss / ax.25 processing blocks
-pybombs -vvv install gr-sigmf   #for signal recording and playback
-pybombs -vvv install gr-vcc     #Custom blocks for VCC mission.
-pybombs -vvv install gr-foo     #Custom blocks for VCC mission.
-pybombs -vvv install gr-pdu_utils
-pybombs -vvv install gr-timing_utils
-pybombs -vvv install gr-message_tools
+pybombs -vvv -t target recipes add gr-vtgs-recipes git+https://github.com/vt-gs/recipes.git
+pybombs -vvv -p target install gr-kiss    #kiss / ax.25 processing blocks
+pybombs -vvv -p target install gr-sigmf   #for signal recording and playback
+pybombs -vvv -p target install gr-vcc     #Custom blocks for VCC mission.
+pybombs -vvv -p target install gr-foo     #Custom blocks for VCC mission.
+pybombs -vvv -p target install gr-pdu_utils
+pybombs -vvv -p target install gr-timing_utils
+pybombs -vvv -p target install gr-message_tools
 
 #Test gnuradio install
 source /gnuradio/target/setup_env.sh
